66e1b1b7238bf46938fa8ea1d17555a57d346cd5
Iryna_Novikova <iris_vs@ukr.net>
2025-06-02 00:40:08 +0300

    add pixabay-api.js, main.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,17 @@
-function getImagesByQuery(query) {
-// Ця функція повинна приймати один параметр query (пошукове слово, яке є рядком), 
-// здійснювати HTTP-запит і повертати значення властивості data з отриманої відповіді.
- }
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com';
+const myApiKey = '50600244-46274b1c7bbfed49553e909c7';
+
+export const getImagesByQuery = (query) => {
+    // параметри запиту
+    const params = {
+        key: myApiKey,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horisontal',
+        safesearch: true,
+    };
+   
+    return axios.get('/api/', { params }).then(response => response.data);
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,53 @@
-import './js/pixabay-api.js';
-import './js/render-functions.js';
+// імпортуємо бібліотеку izitoast
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+// // імпортуємо власні функції
+import { getImagesByQuery } from './js/pixabay-api';
+// // import './js/render-functions.js';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', handlerFormSbmt); 
+
+function handlerFormSbmt(evt) { 
+    evt.preventDefault();
+    const textForSearch = formEl.elements['search-text'].value.trim();
+    
+    if (textForSearch !== '') {
+    //   console.log(textForSearch);
+        getImagesByQuery(textForSearch)
+            .then(res => {
+                if (res.hits.length === 0) {
+                    iziToast.show({
+                        title: '',
+                        message: '❌ Sorry, there are no images matching your search query. Please try again!',
+                        backgroundColor: 'rgb(136, 44, 44)',
+                        messageColor: 'rgb(255, 255, 255)',
+                        position: 'topRight',
+                    });
+                } else {
+                    console.log(res);
+                }
+        })
+            .catch(err => {
+                iziToast.show({
+                    title: '',
+                    message: `❌ Sorry, it's some error hear: ${err}`,
+                    backgroundColor: 'rgb(136, 44, 44)',
+                    messageColor: 'rgb(255, 255, 255)',
+                    position: 'topRight',
+                });
+        });
+        
+    } else { 
+        iziToast.show({
+            title: '',
+            message: '❌ Please enter non empty request.',
+            backgroundColor: 'rgb(136, 44, 44)',
+            messageColor: 'rgb(255, 255, 255)',
+            position: 'topRight',
+        });
+    }
+}
