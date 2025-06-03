9d271e860564b32143981887bbf8769adece5561
Iryna-Novikova <iris_vs@ukr.net>
2025-06-03 15:30:55 +0000

    Deploying to gh-pages from @ Iryna-Novikova/goit-js-hw-11@bf87af2db1f93bf8e15381fe852007734df5bd8b 🚀

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import{a as c,S as f,i as a}from"./assets/vendor-CocXUmuy.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&l(i)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function l(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();c.defaults.baseURL="https://pixabay.com";const g="50600244-46274b1c7bbfed49553e909c7",u=t=>{const s={key:g,q:t,image_type:"photo",orientation:"horizontal",safesearch:!0};return c.get("/api/",{params:s}).then(r=>r.data)},d=document.querySelector(".gallery"),m=document.querySelector(".loader"),h=new f(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"});function p(t){return`<li class="gallery-item">
-    <a class="gallery-link" href="${t.webformatURL}">
+    <a class="gallery-link" href="${t.largeImageURL}">
       <img
         class="gallery-image"
-        src="${t.largeImageURL}"
+        src="${t.webformatURL}"
         alt="${t.tags}"
       /> 
     <div class="img-info">
